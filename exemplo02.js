d01c248d380757c4376115cdae2c546f99bfca8d
thalyta-131 <thalytaf171@gmail.com>
2026-08-25 10:38:22 -0300

    Add exemplo02.js with basic arithmetic and comparisons

---
+++ exemplo02.js
@@ -0,0 +1,19 @@
+let valor1 = 20;
+let valor2 = 5;
+
+console.log(valor1 + valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+console.log(valor1 / valor2);
+
+console.log(10%3); 
+
+    console.log(10>5); //true
+    console.log(10<5); //false
+    console.log(10>=10); //true
+    console.log(10===10); //true
+    console.log(10!==5); //true  
+    
+console.log(10>=10 && 20===20);
+console.log(15>20 || 8<10);
+console.log(!(20>10));
